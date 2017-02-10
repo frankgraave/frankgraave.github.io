28bae9dbb65da8d03d49536923083c1efe491a81
Frank Graave <frankgraave@gmail.com>
2017-02-10 14:19:01 +0100

    init

---
+++ sw.js
@@ -0,0 +1,29 @@
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open('v1').then(cache => {
+      return cache.addAll([
+        '/',
+        '/index.html',
+        '/styles.css',
+        '/images/android-desktop.png',
+        '/images/dog.png',
+        '/images/favicon.png',
+        '/images/ios-desktop.png',
+        '/images/user.jpg',
+      ])
+      .then(() => self.skipWaiting());
+    })
+  )
+});
+
+self.addEventListener('activate',  event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => {
+      return response || fetch(event.request);
+    })
+  );
+});
