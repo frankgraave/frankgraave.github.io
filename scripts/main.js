79e04a0bffeca92170bb80af52f26ce286714c40
Frank Graave <frankgraave@gmail.com>
2017-07-07 09:47:50 +0200

    final version

--- scripts/main.js
+++ scripts/main.js
@@ -1,71 +1,23 @@
 (function() {
   'use strict';
-  //
-  // // Check to make sure service workers are supported in the current browser,
-  // // and that the current page is accessed from a secure origin. Using a
-  // // service worker from an insecure origin will trigger JS console errors. See
-  // // http://www.chromium.org/Home/chromium-security/prefer-secure-origins-for-powerful-new-features
-  // var isLocalhost = Boolean(window.location.hostname === 'localhost' ||
-  //     // [::1] is the IPv6 localhost address.
-  //     window.location.hostname === '[::1]' ||
-  //     // 127.0.0.1/8 is considered localhost for IPv4.
-  //     window.location.hostname.match(
-  //       /^127(?:\.(?:25[0-5]|2[0-4][0-9]|[01]?[0-9][0-9]?)){3}$/
-  //     )
-  //   );
-  //
-  // if ('serviceWorker' in navigator &&
-  //     (window.location.protocol === 'https:' || isLocalhost)) {
-  //       console.log('Registering ServiceWorker...');
-  //   navigator.serviceWorker.register('sw.js')
-  //   .then(function(registration) {
-  //     // updatefound is fired if service-worker.js changes.
-  //     registration.onupdatefound = function() {
-  //       // updatefound is also fired the very first time the SW is installed,
-  //       // and there's no need to prompt for a reload at that point.
-  //       // So check here to see if the page is already controlled,
-  //       // i.e. whether there's an existing service worker.
-  //       if (navigator.serviceWorker.controller) {
-  //         // The updatefound event implies that registration.installing is set:
-  //         // https://slightlyoff.github.io/ServiceWorker/spec/service_worker/index.html#service-worker-container-updatefound-event
-  //         var installingWorker = registration.installing;
-  //
-  //         installingWorker.onstatechange = function() {
-  //           switch (installingWorker.state) {
-  //             case 'installed':
-  //               // At this point, the old content will have been purged and the
-  //               // fresh content will have been added to the cache.
-  //               // It's the perfect time to display a "New content is
-  //               // available; please refresh." message in the page's interface.
-  //               break;
-  //
-  //             case 'redundant':
-  //               throw new Error('The installing ' +
-  //                               'service worker became redundant.');
-  //
-  //             default:
-  //               // Ignore
-  //           }
-  //         };
-  //       }
-  //     };
-  //   }).catch(function(e) {
-  //     console.error('Error during service worker registration:', e);
-  //   });
-  // }
 
+  // Register the ServiceWorker
   if ('serviceWorker' in navigator) {
-
     navigator.serviceWorker
       .register('./sw.js', { scope: './' })
       .then(function(registration) {
-        console.log("[ServiceWorker] Registered");
+        console.log("[MAIN] - SW Registered");
       })
       .catch(function(err) {
-        console.log("Service Worker Failed to Register", err);
+        console.log("[MAIN] - Service Worker Failed to Register", err);
       })
-
   }
+
+  // Cache versioning
+  var CACHE_VERSION = 1;
+  var CURRENT_CACHE = {
+    prefetch: 'fetch-cache-v' + CACHE_VERSION
+  };
 
   // Hide stuff by default
   $('#p2').hide();
@@ -77,55 +29,60 @@
   // Prepare constructors
   var src = $('<img>');
 
-  function beast(){
-    console.log('test');
-  };
-
   $('.mdl-button').click(function(e) {
-    e.preventDefault();
+    // Disabled for now since it will request images that aren't there.
+    // var resultsLimit = $('#sliderValue').val();
+    // var randomNr = Math.floor(Math.random() * resultsLimit + 1);
 
-    // Take this actual value upon clicking the button
-    var resultsLimit = $('#sliderValue').val();
-    var randomNr = Math.floor(Math.random() * resultsLimit + 1);
-    // Clear the previous searched gif
+    // Clear the previous searched gif in the container
     $('#img-container').empty();
+
     // Show the loader until the data arrives
     $('#p2').show();
+
+    // Do the XHR request
     var xhr = $.ajax(
-      apiUrl + $('.search-query').val() + '&api_key=' + apiKey + '&limit=' + resultsLimit
+      apiUrl + $('.search-query').val() + '&api_key=' + apiKey + '&limit=0'
     );
+
+    // Set headers
     xhr.setRequestHeader("X-Requested-With", "XMLHttpRequest");
     xhr.setRequestHeader('Access-Control-Allow-Origin', '*');
+
     // Some error handling
     window.addEventListener('error', function (e) {
       var error = e.error;
       $('.error').html('Someting went wrong with the request, or you haven\'t filled out any search term.');
-      console.log(error);
+      console.log('[MAIN] - ', error);
     });
+    xhr.fail(function(){
+      $('#p2').fadeOut();
+      $('.error').html('Sorry, the server returned an error upon your request. Please try again!');
+    })
+
+    // We've got the data
     xhr.done(function(data) {
-      caches.open('v1').then(function(cache) {
-        fetch(apiUrl + $('.search-query').val() + '&api_key=' + apiKey + '&limit=' + resultsLimit).then(function(response) {
-          // /get-article-urls returns a JSON-encoded array of
-          // resource URLs that a given article depends on
-          console.log(response.url);
-          return response;
-        }).then(function(response) {
-          console.log('Caching...', data.data[randomNr-1].images.original.url);
-          cache.addAll([data.data[randomNr-1].images.original.url, response.url]);
-          console.log('Caching Successfully Done');
-        });
-      });
+      // Hide stuff
       $('.error').html('');
       $('#p2').fadeOut();
+
+      // Open the cache because we need to store the data from the XHR request
+      caches.open(CURRENT_CACHE).then(function(cache) {
+        fetch(apiUrl + $('.search-query').val() + '&api_key=' + apiKey + '&limit=0').then(function(response) {
+          return response;
+        }).then(function(response) {
+          console.log('[MAIN] - New data is cached');
+          cache.addAll([data.data[0].images.original.url, response.url]);
+        });
+      });
+
       // console.log('Successfully got data: ', data);
       // console.log(data.data[randomNr].slug);
       var img = $('<img />').attr({
           'id': 'gif',
           'class': '.mdl-shadow--2dp',
-          'src': data.data[randomNr-1].images.original.url,
-          // 'alt': data.data[randomNr++].slug,
-          // 'title': data.data[randomNr++].slug,
-        }).appendTo('#img-container, #img-container-history');
+          'src': data.data[0].images.original.url,
+        }).appendTo('#img-container');
       });
   });
 
--- sw.js
+++ sw.js
@@ -1,88 +1,205 @@
-// Set a name for the current cache
-var cacheName = 'v1';
+// // Set a name for the current cache
+// var cacheName = 'v1';
+//
+// // Default files to always cache
+// var cacheFiles = [
+// 	'./',
+// 	'./?#',
+//   'index.html',
+// 	'styles/material-indigo-pink-min.css',
+//   'styles/main.css',
+//   // 'scripts/main.js',
+//   'scripts/main.min.js',
+// 	'scripts/material.min.js',
+// 	'scripts/jquery-3.2.1.min.js',
+//   'images/touch/apple-touch-icon.png',
+//   'images/touch/chrome-touch-icon-192x192.png',
+//   'images/touch/icon-128x128.png',
+//   'images/touch/ms-touch-icon-144x144-precomposed.png',
+//   'images/hamburger.svg',
+// ];
+//
+// self.addEventListener('install', function(event) {
+//   console.log('[ServiceWorker] Installed');
+//   // event.waitUntil Delays the event until the Promise is resolved
+//   event.waitUntil(
+//   	// Open the cache
+//     caches.open(cacheName).then(function(cache) {
+//     // Add all the default files to the cache
+// 		console.log('[ServiceWorker] Caching cacheFiles', cacheFiles);
+// 		return cache.addAll(cacheFiles);
+//     })
+// 	); // end event.waitUntil
+// 	console.log('[ServiceWorker] Done Caching Files');
+// });
+//
+//
+// self.addEventListener('activate', function(event) {
+// 	self.clients.claim();
+//     console.log('[ServiceWorker] Activated');
+//     event.waitUntil(
+//   	// Get all the cache keys (cacheName)
+// 		caches.keys().then(function(cacheNames) {
+// 			return Promise.all(cacheNames.map(function(thisCacheName) {
+// 				// If a cached item is saved under a previous cacheName
+// 				if (thisCacheName !== cacheName) {
+// 					// Delete that cached file
+// 					console.log('[ServiceWorker] Removing Cached Files from Cache - ', thisCacheName);
+// 					return caches.delete(thisCacheName);
+// 				}
+// 			}));
+// 		})
+// 	); // end event.waitUntil
+// });
+//
+// self.addEventListener('fetch', function(event) {
+// 	console.log('[ServiceWorker] Fetching', event.request.url);
+// 	// event.respondWidth Responds to the fetch event
+// 	event.respondWith(
+// 		// Check in cache for the request being made
+// 		caches.match(event.request).then(function(response) {
+// 			// If the request is in the cache
+// 			if ( response ) {
+// 				// console.log("[ServiceWorker] Request found in Cache", event.request.url, response);
+// 				// Return the cached version
+// 				return response;
+// 			}
+// 			// If the request is NOT in the cache, fetch and cache
+// 			var requestClone = event.request.clone();
+// 			fetch(requestClone).then(function(response) {
+// 					if ( !response ) {
+// 						console.log("[ServiceWorker] No response from fetch")
+// 						return response;
+// 					}
+// 					var responseClone = response.clone();
+// 					//  Open the cache
+// 					console.log('[ServiceWorker] Opening the cache...');
+// 					caches.open(cacheName).then(function(cache) {
+// 						// Put the fetched response in the cache
+// 						cache.put(event.request, responseClone);
+// 						console.log('[ServiceWorker] New Data Cached', event.request.url);
+// 						// Return the response
+// 						return response;
+// 	        }); // end caches.open
+// 				}).catch(function(err) {
+// 					console.log('[ServiceWorker] Error Fetching & Caching New Data', err);
+// 				});
+// 			}) // end caches.match(event.request)
+// 	); // end event.respondWith
+// });
 
-// Default files to always cache
-var cacheFiles = [
-	'./',
-  'index.html',
-	'styles/material-indigo-pink-min.css',
-  'styles/main.css',
-  // 'scripts/main.js',
-  'scripts/main.min.js',
-	'scripts/material.min.js',
-	'scripts/jquery-3.2.1.min.js',
-  'images/touch/apple-touch-icon.png',
-  'images/touch/chrome-touch-icon-192x192.png',
-  'images/touch/icon-128x128.png',
-  'images/touch/ms-touch-icon-144x144-precomposed.png',
-  'images/hamburger.svg',
-];
 
-self.addEventListener('install', function(e) {
-  console.log('[ServiceWorker] Installed');
-  // e.waitUntil Delays the event until the Promise is resolved
-  e.waitUntil(
-  	// Open the cache
-    caches.open(cacheName).then(function(cache) {
-    // Add all the default files to the cache
-		console.log('[ServiceWorker] Caching cacheFiles', cacheFiles);
-		return cache.addAll(cacheFiles);
+var CACHE_VERSION = 1;
+var CURRENT_CACHE = {
+  prefetch: 'prefetch-cache-v' + CACHE_VERSION
+};
+
+self.addEventListener('install', function(event) {
+  var now = Date.now();
+
+  var urlsToPrefetch = [
+		'./',
+		'./?#',
+	  'index.html',
+		'styles/material-indigo-pink-min.css',
+	  'styles/main.css',
+	  'scripts/main.js',
+	  'scripts/main.min.js',
+		'scripts/material.min.js',
+		'scripts/jquery-3.2.1.min.js',
+	  'images/touch/apple-touch-icon.png',
+	  'images/touch/chrome-touch-icon-192x192.png',
+	  'images/touch/icon-128x128.png',
+	  'images/touch/ms-touch-icon-144x144-precomposed.png',
+	  'images/hamburger.svg',
+  ];
+
+  console.log('[SW] - Handling install event. Resources to prefetch:', urlsToPrefetch);
+
+  event.waitUntil(
+    caches.open(CURRENT_CACHE.prefetch).then(function(cache) {
+      var cachePromises = urlsToPrefetch.map(function(urlToPrefetch) {
+        // This constructs a new URL object using the service worker's script location as the base
+        // for relative URLs.
+        var url = new URL(urlToPrefetch, location.href);
+        // IMPORTANT: Append a cache-bust=TIMESTAMP URL parameter to each URL's query string.
+        // This is particularly important when precaching resources that are later used in the
+        // fetch handler as responses directly, without consulting the network (i.e. cache-first).
+        url.search += (url.search ? '&' : '?') + 'cache-bust=' + now;
+
+        var request = new Request(url, {mode: 'no-cors'});
+        return fetch(request).then(function(response) {
+          if (response.status >= 400) {
+            throw new Error('[SW] - Request for ' + urlToPrefetch +
+              ' failed with status ' + response.statusText);
+          }
+
+          // Use the original URL without the cache-busting parameter as the key for cache.put().
+          return cache.put(urlToPrefetch, response);
+        }).catch(function(error) {
+          console.error('[SW] - Not caching ' + urlToPrefetch + ' due to ' + error);
+        });
+      });
+
+      return Promise.all(cachePromises).then(function() {
+        console.log('[SW] - Pre-fetching complete.');
+      });
+    }).catch(function(error) {
+      console.error('[SW] - Pre-fetching failed:', error);
     })
-	); // end e.waitUntil
-	console.log('[ServiceWorker] Done Caching Files');
+  );
 });
 
+self.addEventListener('activate', function(event) {
+  // Delete all caches that aren't named in CURRENT_CACHE.
+  var expectedCacheNames = Object.keys(CURRENT_CACHE).map(function(key) {
+    return CURRENT_CACHE[key];
+  });
 
-self.addEventListener('activate', function(e) {
-	self.clients.claim();
-    console.log('[ServiceWorker] Activated');
-    e.waitUntil(
-  	// Get all the cache keys (cacheName)
-		caches.keys().then(function(cacheNames) {
-			return Promise.all(cacheNames.map(function(thisCacheName) {
-				// If a cached item is saved under a previous cacheName
-				if (thisCacheName !== cacheName) {
-					// Delete that cached file
-					console.log('[ServiceWorker] Removing Cached Files from Cache - ', thisCacheName);
-					return caches.delete(thisCacheName);
-				}
-			}));
-		})
-	); // end e.waitUntil
+  event.waitUntil(
+    caches.keys().then(function(cacheNames) {
+      return Promise.all(
+        cacheNames.map(function(cacheName) {
+          if (expectedCacheNames.indexOf(cacheName) === -1) {
+            // If this cache name isn't present in the array of "expected" cache names, then delete it.
+            console.log('[SW] - Deleting out of date cache:', cacheName);
+            return caches.delete(cacheName);
+          }
+        })
+      );
+    })
+  );
 });
 
-self.addEventListener('fetch', function(e) {
-	console.log('[ServiceWorker] Fetch', e.request.url);
-	// e.respondWidth Responds to the fetch event
-	e.respondWith(
-		// Check in cache for the request being made
-		caches.match(e.request).then(function(response) {
-			// If the request is in the cache
-			if ( response ) {
-				console.log("[ServiceWorker] Found in Cache", e.request.url, response);
-				// Return the cached version
-				return response;
-			}
-			// If the request is NOT in the cache, fetch and cache
-			var requestClone = e.request.clone();
-			fetch(requestClone)
-				.then(function(response) {
-					if ( !response ) {
-						console.log("[ServiceWorker] No response from fetch ")
-						return response;
-					}
-					var responseClone = response.clone();
-					//  Open the cache
-					caches.open(cacheName).then(function(cache) {
-						// Put the fetched response in the cache
-						cache.put(e.request, responseClone);
-						console.log('[ServiceWorker] New Data Cached', e.request.url);
-						// Return the response
-						return response;
-	        }); // end caches.open
-				}).catch(function(err) {
-					console.log('[ServiceWorker] Error Fetching & Caching New Data', err);
-				});
-			}) // end caches.match(e.request)
-	); // end e.respondWith
+self.addEventListener('fetch', function(event) {
+  console.log('[SW] - Handling fetch event for', event.request.url);
+
+  event.respondWith(
+    // caches.match() will look for a cache entry in all of the caches available to the service worker.
+    // It's an alternative to first opening a specific named cache and then matching on that.
+    caches.match(event.request).then(function(response) {
+      if (response) {
+        console.log('[SW] - Found response in cache:', response);
+
+        return response;
+      }
+
+      console.log('[SW] - No response found in cache. About to fetch from network...');
+
+      // event.request will always have the proper mode set ('cors, 'no-cors', etc.) so we don't
+      // have to hardcode 'no-cors' like we do when fetch()ing in the install handler.
+      return fetch(event.request).then(function(response) {
+        console.log('[SW] - Response from network is:', response);
+
+        return response;
+      }).catch(function(error) {
+        // This catch() will handle exceptions thrown from the fetch() operation.
+        // Note that a HTTP error response (e.g. 404) will NOT trigger an exception.
+        // It will return a normal response object that has the appropriate error code set.
+        console.error('[SW] - Fetching failed:', error);
+
+        throw error;
+      });
+    })
+  );
 });
